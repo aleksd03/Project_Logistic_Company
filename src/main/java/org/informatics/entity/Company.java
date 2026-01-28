ce8f080fbaf5ea1b5d3cf87690049cc9f44209b4
ZackFreiday <mutafchiev4000@gmail.com>
2026-01-28 02:47:39 +0200

    Update Company.java

--- src/main/java/org/informatics/entity/Company.java
+++ src/main/java/org/informatics/entity/Company.java
@@ -9,6 +9,10 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Entity representing a Company.
+ * A Company may have multiple Offices and multiple Clients.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,12 +21,28 @@ import java.util.List;
 @Table(name = "companies")
 public class Company extends BaseEntity {
 
+    /**
+     * Name of the company.
+     * Cannot be null.
+     */
     @Column(nullable = false)
     private String name;
 
+    /**
+     * One-to-many relationship with Office.
+     * - A Company can have multiple Offices
+     * - CascadeType.ALL propagates persistence operations
+     * - orphanRemoval removes Offices when they are detached from the Company
+     */
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Office> offices = new ArrayList<>();
 
+    /**
+     * One-to-many relationship with Client.
+     * - A Company can have multiple Clients
+     * - No cascading to avoid accidental deletion of clients
+     */
     @OneToMany(mappedBy = "company")
     private List<Client> clients = new ArrayList<>();
 }
+
