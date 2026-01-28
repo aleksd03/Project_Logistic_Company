ddefa00be59f33f8f19a16f212cbb20e9caecef6
ZackFreiday <mutafchiev4000@gmail.com>
2026-01-28 02:43:08 +0200

    Update UserLoginDto.java

--- src/main/java/org/informatics/dto/UserLoginDto.java
+++ src/main/java/org/informatics/dto/UserLoginDto.java
@@ -2,11 +2,21 @@ package org.informatics.dto;
 
 import lombok.*;
 
+/**
+ * Data Transfer Object (DTO) used for user authentication.
+ * Carries login credentials from the client to the service layer.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginDto {
+
+    // User email used for authentication
     private String email;
+
+    // Plain-text password submitted during login
+    // (will be validated and compared after hashing)
     private String password;
 }
+
